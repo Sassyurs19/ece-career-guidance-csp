898d9d312b09f973c8913dd9f0eaa3a2cc4005e8
Munnangi Sasidhar <sassyurs19@gmail.com>
2026-06-21 10:09:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("ECE Career Explorer Loaded");
+
+function checkQuiz(){
+
+    let score = 0;
+
+    const answers =
+    document.querySelectorAll(
+    'input[value="correct"]:checked'
+    );
+
+    score = answers.length;
+
+    let result =
+    document.getElementById("quiz-result");
+
+    if(score === 5){
+
+        result.innerHTML =
+        "🌟 Excellent! Score: 5/5";
+
+    }
+
+    else if(score >= 3){
+
+        result.innerHTML =
+        "🚀 Great Job! Score: " + score + "/5";
+
+    }
+
+    else{
+
+        result.innerHTML =
+        "📚 Nice Try! Score: " + score + "/5";
+
+    }
+}
